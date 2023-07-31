7aad030690115006036b676466c0f1e561b1cea5
zthCn <1119115587@qq.com>
2023-07-31 18:47:07 +0800

    fix: test changelog

--- src/plugins/after/success.ts
+++ src/plugins/after/success.ts
@@ -2,7 +2,7 @@ import type { TContext, TPlugin } from '../../utils'
 
 const success: TPlugin = async function (ctx: TContext) {
   ctx.shared = {}
-  ctx.log?.('COSTOM', 'green', '执行成功~~,程序即将退出')
+  ctx.log?.('COSTOM', 'green', '执行成功~~,程序即将退出!')
 }
 
 success.lifecycle = 'success'
